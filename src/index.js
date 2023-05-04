555779f28cbbc8ff32fd91ecaa7b45e4b318e88d
Enoisong <enobongitiaba988@gmail.com>
2023-05-04 03:04:18 -0700

    fix linters

--- src/index.js
+++ src/index.js
@@ -1,22 +1,20 @@
-import './style.css'; 
+import './style.css';
 
 const todosList = [
-  {description: 'todo2', completed: false, index: 2},
-  {description: 'todo1', completed: false, index: 1 },
-]; 
+  { description: 'todo2', completed: false, index: 2 },
+  { description: 'todo1', completed: false, index: 1 },
+];
 
 const render = (list) => {
-    const sortedTodos = list.sort((a, b) => a.index - b.index);
-    const todosContainer = document.querySelector('.todos');
-    let todosHtml = '';
-    sortedTodos.forEach((todo) => {
-        todosHtml += `<div class="todo-item">
+  const sortedTodos = list.sort((a, b) => a.index - b.index);
+  const todosContainer = document.querySelector('.todos');
+  let todosHtml = '';
+  sortedTodos.forEach((todo) => {
+    todosHtml += `<div class="todo-item">
         <input type="checkbox" /><span> ${todo.description}</span>        
         </div>`;
-    });
-    todosContainer.innerHTML = todosHtml;
+  });
+  todosContainer.innerHTML = todosHtml;
 };
 
 render(todosList);
-
- 
