96dbbbb8e2ddfa32fba48871d7a0c1ee891ac68d
Enoisong <enobongitiaba988@gmail.com>
2023-05-10 02:53:12 -0700

    edit jest

--- src/__mock__/todos.test.js
+++ src/__mock__/todos.test.js
@@ -4,44 +4,42 @@
 import Todos from '../todos.js';
 
 describe('add and remove', () => {
-    window.localStorage = Storage.prototype;
-    test('Add task', () => {
-        const todoList = new Todos();
-        const newTodo = {
-            id: 'id123',
-            description: 'task1',
-            completed: false,
-            index: 1,
-        };
+  window.localStorage = Storage.prototype;
+  test('Add task', () => {
+    const todoList = new Todos();
+    const newTodo = {
+      id: 'id123',
+      description: 'task1',
+      completed: false,
+      index: 1,
+    };
 
-        const newTodo2 = {
-            id: 'id12sdad3',
-            description: 'task2',
-            completed: false,
-            index: 2,
-        };
-        
-      todoList.addTodo(newTodo);
-      expect(todoList.list).toHaveLength(1);
-      todoList.addTodo(newTodo2);
-      expect(todoList.list).toHaveLength(2);
-      expect(todoList.list[1].description).toBe('task2');     
-      });
+    const newTodo2 = {
+      id: 'id12sdad3',
+      description: 'task2',
+      completed: false,
+      index: 2,
+    };
 
-    test('remove task', () => {
-      const todoList = new Todos();
-      const newTodo = {
-        id: 'id4d5sa',
-        description: 'task3',
-        completed: false,
-        index: 3,
-       };
+    todoList.addTodo(newTodo);
+    expect(todoList.list).toHaveLength(1);
+    todoList.addTodo(newTodo2);
+    expect(todoList.list).toHaveLength(2);
+    expect(todoList.list[1].description).toBe('task2');
+  });
 
-        todoList.addTodo(newTodo);
-        todoList.removeTodo(newTodo.id);
-        expect(todoList.list[1].description).toBe('task2');
-        expect(todoList.list).toHaveLength(2);
-      });      
-    });
+  test('remove task', () => {
+    const todoList = new Todos();
+    const newTodo = {
+      id: 'id4d5sa',
+      description: 'task3',
+      completed: false,
+      index: 3,
+    };
 
-     
+    todoList.addTodo(newTodo);
+    todoList.removeTodo(newTodo.id);
+    expect(todoList.list[1].description).toBe('task2');
+    expect(todoList.list).toHaveLength(2);
+  });
+});
